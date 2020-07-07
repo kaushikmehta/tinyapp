94c9d79b9bfa701f9f6c1a0599909411d65d0cc6
Kaushik Mehta <kaushikmehta30@gmail.com>
2020-07-07 00:14:12 +0000

    added route and template for new url submission

--- express_server.js
+++ express_server.js
@@ -23,12 +23,17 @@ app.get("/urls.json", (req, res) => {
 app.get("/urls", (req, res) => {
   let templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);
-})
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {shortURL: req.params.shortURL, longURL: req.params.longURL};
   res.render("urls_show", templateVars);
-})
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
