185e8d5a182ef07ab96717cf7d3bd5b8805e5fd7
Kaushik Mehta <kaushikmehta30@gmail.com>
2020-07-07 16:59:00 +0000

    added delete button and route to post deletion

--- express_server.js
+++ express_server.js
@@ -59,6 +59,12 @@ app.post("/urls", (req, res) => {
   res.redirect(req.body.longURL);;         // Respond with 'Ok' (we will replace this)
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  console.log(urlDatabase);
+  res.redirect('/urls');
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
